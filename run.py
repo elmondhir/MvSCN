d0b0374830e28f471b75f54681d67836598f2a8e
CHAALAL Mohamed Elmondhir <94637607+elmondhir@users.noreply.github.com>
2024-02-16 12:45:59 +0100

    test

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ sweep_config = {
     'method': 'random',
     "metric": {'goal': 'minimize', 'name': 'loss'},
     'parameters': {
-                'epochs': {'values': [2, 3]},
+                'epochs': {'values': [50, 100]},
                 'siam_k':{'values': [2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18]},
                 'lamb':{'values': [0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001 , 0.0005, 0.00001]},
                 }
